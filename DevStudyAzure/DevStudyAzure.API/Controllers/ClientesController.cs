06f847d8d386369ad027c1e48961f83aba1ae3ca
Diogo Salvador <105182603+diogosalvadorb@users.noreply.github.com>
2024-09-29 19:55:22 -0300

    retorno endpoints

---
+++ DevStudyAzure/DevStudyAzure.API/Controllers/ClientesController.cs
@@ -0,0 +1,47 @@
+﻿using DevStudyAzure.API.Data;
+using DevStudyAzure.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevStudyAzure.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private Context _context;
+        public ClientesController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Cliente> GetAll()
+        {
+            return _context.Clientes;
+        }
+
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] Cliente clienteModel)
+        {
+            var clienteNovo = _context.Clientes.Add(clienteModel);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var filme = _context.Clientes.SingleOrDefault(filme => filme.Id == id);
+
+            if (filme == null) return NotFound();
+
+            _context.Remove(filme);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
