65d53219ad5f87075973e6c290bc7484c7c3e43a
Diogo Salvador <105182603+diogosalvadorb@users.noreply.github.com>
2024-09-29 19:44:27 -0300

    API com acesso ao banco local

---
+++ DevStudyAzure/DevStudyAzure.API/Data/Context.cs
@@ -0,0 +1,12 @@
+﻿using DevStudyAzure.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevStudyAzure.API.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ DevStudyAzure/DevStudyAzure.API/Models/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace DevStudyAzure.API.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+    }
+}
